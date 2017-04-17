14e2307b7560edb399f4e5cec6793e2394ec8b65
sergicurtu <sergi.curtu@numancia.cat>
2017-04-17 16:43:59 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -90,7 +90,7 @@ $(document).on('deviceready', function() {
 		
 			draw(estat_joc,mida_x_bola,mida_y_bola,posicio_x_bola,posicio_y_bola) ;
 			estat_joc = 1 ; 
-			setInterval(dibuixar_bola, 10);
+			var loop_bola=setInterval(dibuixar_bola, 10);
 		
 		}
 		
